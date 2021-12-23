26e3561260b2a08d3473eff7b2f60da6f78f312e
d3b0 <94570632+arnaudebo@users.noreply.github.com>
2021-12-23 01:21:49 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import pyautogui
+import time
+
+mensaje = input("Que mensaje quieres spamear:\n ")
+numveces = int(input("Quantas veces?:\n "))
+delay = int(input("Con quanto delay?\n "))
+
+isLoaded = input("\n\nPulsa [Enter] quando tengas el programa abierto")
+
+print("\nTenes 10 segundos antes de que empieze")
+time.sleep(10)
+for i in range(0,numveces):
+    if mensaje != "":
+        pyautogui.typewrite(mensaje)
+        pyautogui.press("enter")
+    else:
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.press("enter")
+    time.sleep(delay/1000)
+
+print("\n ya esta ;)")
