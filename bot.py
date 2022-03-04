7497ca37566094daf5613f80aee6e4779707a31e
DeBo <94570632+d3b0o@users.noreply.github.com>
2022-03-04 13:04:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,17 +5,21 @@ mensaje = input("Que mensaje quieres spamear:\n ")
 numveces = int(input("Quantas veces?:\n "))
 delay = int(input("Con quanto delay?\n "))
 
-isLoaded = input("\n\nPulsa [Enter] quando tengas el programa abierto")
+isLoaded = input("\n\nPulsa [Enter] para que epiece la cuenta regresiva de 10 segundos")
+time = 0
+while True:
+    time += 1
+    if time == 11:
+        for i in range(0,numveces):
+            if mensaje != "":
+                pyautogui.typewrite(mensaje)
+                pyautogui.press("enter")
+            else:
+                pyautogui.hotkey('ctrl', 'v')
+                pyautogui.press("enter")
+            time.sleep(delay/1000)
 
-print("\nTienes 10 segundos antes de que empieze")
-time.sleep(10)
-for i in range(0,numveces):
-    if mensaje != "":
-        pyautogui.typewrite(mensaje)
-        pyautogui.press("enter")
-    else:
-        pyautogui.hotkey('ctrl', 'v')
-        pyautogui.press("enter")
-    time.sleep(delay/1000)
-
-print("\n ya esta ;)")
+        print("\n ya esta ;)")
+        break
+       else:
+        print(time)
